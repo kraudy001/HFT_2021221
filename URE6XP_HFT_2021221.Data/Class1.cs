f45094e2948d7ec6ff21ade0ff8dca389af908a6
kraudy001 <90103526+kraudy001@users.noreply.github.com>
2021-10-04 15:26:09 +0200

    Add project files.

---
+++ URE6XP_HFT_2021221.Data/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace URE6XP_HFT_2021221.Data
+{
+    public class Class1
+    {
+    }
+}
---
+++ URE6XP_HFT_2021221.Logic/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace URE6XP_HFT_2021221.Logic
+{
+    public class Class1
+    {
+    }
+}
