ed00d4d6e9bf36e0759b49b30e0b8d99397935b7
kraudy001 <90103526+kraudy001@users.noreply.github.com>
2021-11-27 16:17:13 +0100

    Test Fix

--- URE6XP_HFT_2021221.Test/Test.cs
+++ URE6XP_HFT_2021221.Test/Test.cs
@@ -38,12 +38,12 @@ namespace URE6XP_HFT_2021221.Test
 
 
 
-            var HFT = new Presentation() { PresentationName = "HFT", LectureHall = BAF01, RoomNumber = BAF01.RoomNumber, Instructor = KovacsAndras, InstrctoreName = KovacsAndras.Name };
-            var SZTF1 = new Presentation() { PresentationName = "SZTF1", LectureHall = BA132, RoomNumber = BA132.RoomNumber, Instructor = KovacsAndras, InstrctoreName = KovacsAndras.Name };
-            var Archi1=  new Presentation() { PresentationName = "Archi 1", LectureHall = BAF01, RoomNumber = BAF01.RoomNumber, Instructor = DurczyLevente, InstrctoreName = DurczyLevente.Name };
-            var VIR = new Presentation() { PresentationName = "VIR", LectureHall = BC3304, RoomNumber = BC3304.RoomNumber, Instructor = DrHolynkaPeter, InstrctoreName = DrHolynkaPeter.Name };
-            var Menedzsment = new Presentation() { PresentationName = "Menedzsment alapjai", LectureHall = BA210, RoomNumber = BA210.RoomNumber, Instructor = DrBujdosoLaszlo, InstrctoreName = DrBujdosoLaszlo.Name };
-            var Angol1 = new Presentation() { PresentationName = "Angol Szaknyel A", LectureHall = BC3304, RoomNumber = BC3304.RoomNumber, Instructor = GyorineKontorEva, InstrctoreName = GyorineKontorEva.Name };
+            var HFT = new Presentation() { PresentationName = "HFT", LectureHall = BAF01, RoomNumber = BAF01.RoomNumber, Instructor = KovacsAndras, InstrctoreNeptunId = KovacsAndras.NeptunId };
+            var SZTF1 = new Presentation() { PresentationName = "SZTF1", LectureHall = BA132, RoomNumber = BA132.RoomNumber, Instructor = KovacsAndras, InstrctoreNeptunId = KovacsAndras.NeptunId };
+            var Archi1=  new Presentation() { PresentationName = "Archi 1", LectureHall = BAF01, RoomNumber = BAF01.RoomNumber, Instructor = DurczyLevente, InstrctoreNeptunId = DurczyLevente.NeptunId };
+            var VIR = new Presentation() { PresentationName = "VIR", LectureHall = BC3304, RoomNumber = BC3304.RoomNumber, Instructor = DrHolynkaPeter, InstrctoreNeptunId = DrHolynkaPeter.NeptunId };
+            var Menedzsment = new Presentation() { PresentationName = "Menedzsment alapjai", LectureHall = BA210, RoomNumber = BA210.RoomNumber, Instructor = DrBujdosoLaszlo, InstrctoreNeptunId = DrBujdosoLaszlo.NeptunId };
+            var Angol1 = new Presentation() { PresentationName = "Angol Szaknyel A", LectureHall = BC3304, RoomNumber = BC3304.RoomNumber, Instructor = GyorineKontorEva, InstrctoreNeptunId = GyorineKontorEva.NeptunId };
 
             BAF01.Presentations = new List<Presentation> { HFT, Archi1 };
             BA132.Presentations = new List<Presentation> { SZTF1 };
@@ -60,10 +60,11 @@ namespace URE6XP_HFT_2021221.Test
 
             var lectureHalls = new List<LectureHall>() { BA115, BA132, BA210, BAF01, BC3304 }.AsQueryable();
             mockLectureHallRepository.Setup((t) => t.ReadALL()).Returns(lectureHalls);
+            mockLectureHallRepository.Setup((t) => t.Read(It.IsAny<string>())).Returns(BA115);
 
             var instructirs = new List<Instructor>() { KovacsAndras, DrBujdosoLaszlo, DrHolynkaPeter, SimonNagyGabriella, DurczyLevente, GyorineKontorEva }.AsQueryable();
             mockInstructorLogicRepositroy.Setup((t) => t.ReadALL()).Returns(instructirs);
-            mockInstructorLogicRepositroy.Setup((t) => t.Read(It.IsAny<string>())).Returns(instructirs.FirstOrDefault(x => x.NeptunId == It.IsAny<string>()));
+            mockInstructorLogicRepositroy.Setup((t) => t.Read(It.IsAny<string>())).Returns(KovacsAndras);
 
             var presentations = new List<Presentation>() { HFT,SZTF1,Archi1,VIR,Menedzsment,Angol1 }.AsQueryable();
             mockPresentationRepository.Setup((t) => t.ReadALL()).Returns(presentations);
@@ -147,7 +148,7 @@ namespace URE6XP_HFT_2021221.Test
         { 
             Assert.That(
 
-                () => presentationLogic.Create(new Presentation { PresentationName = "NO", InstrctoreName = "Még Egy Előadó Név", RoomNumber = "AB.1.12" }),
+                () => presentationLogic.Create(new Presentation { PresentationName = "NO", InstrctoreNeptunId = "Még Egy Előadó Név", RoomNumber = "AB.1.12" }),
 
                 Throws.TypeOf<ArgumentException>()
                 
@@ -176,35 +177,20 @@ namespace URE6XP_HFT_2021221.Test
                 );
         }
 
-
-        //[Test]
-        //public void InstructorUpdate()
-        //{
-        //    //ACT
-        //    var valamik = instructorLogic.Read("DFVW5VD");
-
-
-        //    instructorLogic.Update(new Instructor() { Name = "Dr.Kovács András", NeptunId = "DFVW5VD" });
-
-        //    //ASSERT
-
-        //    Assert.That("Dr.Kovács András", Is.EqualTo(instructorLogic.Read("DFVW5VD").Name));   //hát én ezt nem értem :(
-        //}
-
         [Test]
-        public void LectureHallLogicDeleteTest()
+        public void LectureHallLogicReadTest()
         {
-            lectureHallLogic.Delete("BA.1.15");
+            var item = lectureHallLogic.Read("BA.1.15");
 
-            Assert.That(lectureHallLogic.Read("BA.1.15"), Is.Null);
+            Assert.That("BA.1.15", Is.EqualTo(item.RoomNumber));
         }        
 
         [Test]
-        public void InstructorLogicDeletTest()
+        public void InstructorLogicReadTest()
         {
-            lectureHallLogic.Delete("DFVW5VD");
+            var item = instructorLogic.Read("DFVW5VD");
 
-            Assert.That(lectureHallLogic.Read("DFVW5VD"), Is.Null);
+            Assert.That("DFVW5VD", Is.EqualTo(item.NeptunId));
         }
     }
 }
